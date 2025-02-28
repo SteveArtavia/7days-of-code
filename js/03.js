da2d7e851b54e2d6b4e45029aed3e9f6e0068757
Steve Artavia <stvartavia@gmail.com>
2025-02-28 14:50:38 -0600

    dia 3 concluido

--- js/03.js
+++ js/03.js
@@ -3,36 +3,126 @@ const containerPreguntas = document.querySelector('.container__form');
 const containerButton = document.querySelector('.container__form__button');
 
 let nombre = 'usuario';
-
 let controlador = "inicio";
+let tecnologias = [];
 
-function confirmar(nombre) {
+function confirmar(nombreUsuario) {
     nombre = entradaNombre.value;
+    nombreUsuario = nombre;
 
     if(controlador === "inicio") {
-        containerPreguntas.innerHTML = `
-            <h3 class="pregunta">${nombre} quieres estudiar Front-End o Back-End?</h3>
+        preguntasPlantilla(`${nombreUsuario} quieres dedicarte al Front-End o al Back-End?`, 'front','back');
+        return;
 
-            <button class="button-respuesta" id="front" onclick="front();">Front-End</button>
-
-            <button class="button-respuesta" id="back" onclick="controlador='back'">Back-End</button>
-        `;
     } else if(controlador === "front"){
-        containerPreguntas.innerHTML = `
-            <h3 class="pregunta">Quieres aprender React o Vue?</h3>
+        preguntasPlantilla(`Quieres aprender react o vue?`, 'react', 'vue');
+        return;
 
-            <button class="button-respuesta" id="react" onclick="controlador='react'">React</button>
+    } else if(controlador == 'vue'){
+        preguntasPlantilla(`Quieres especializarte en ${controlador} o quieres ser fullstack?`, 'especializacion', 'fullstack');
+        return;
 
-            <button class="button-respuesta" id="vue" onclick="controlador='vue'">Vue</button>
-        `;
+    } else if(controlador == 'react'){
+        preguntasPlantilla(`Quieres especializarte en ${controlador} o quieres ser fullstack?`, 'especializacion', 'fullstack');
+        return;
+
+    } else if(controlador == 'back'){
+        preguntasPlantilla('Quieres aprender C# o Java?', 'csharp', 'java');
+        return;
+
+    } else if(controlador == 'csharp'){
+        preguntasPlantilla(`Quieres especializarte en ${controlador} o quieres ser fullstack?`, 'especializacion', 'fullstack');
+        return;
+
+    } else if(controlador == 'java'){    
+        preguntasPlantilla(`Quieres especializarte en ${controlador} o quieres ser fullstack?`, 'especializacion', 'fullstack');
+        return;
+
+    } else if(controlador == 'especializacion'){
+       containerPreguntas.innerHTML = `Felicidades ${nombreUsuario}, sigue adelante y especializate en tu area de interes`;
+
+       setTimeout(() => {
+            preguntasPlantilla(`${nombre} hay alguna otra tecnologia que te gustaria aprender?`, 'si', 'no');
+       }, 3000);
+        return;
+
+    } else if(controlador == 'fullstack'){
+        containerPreguntas.innerHTML = `Felicidades ${nombreUsuario}, sigue adelante y conviertete en desarrollador fullstack`;
+
+        setTimeout(() => {
+            preguntasPlantilla(`${nombreUsuario} hay alguna otra tecnologia que te gustaria aprender?`, 'si', 'no');
+       }, 3000);
+        return;
+
+    } else if(controlador == 'si'){
+        si(nombreUsuario);
+        return;
+
+    } else if(controlador == 'no'){
+        no(nombreUsuario);
+        return;
     }
+
+    return nombreUsuario;
 }
 
-// al presionar boton
+// al presionar boton inicial para confirmar nombre
 containerButton.addEventListener('click', confirmar);
 
-function front(){
-    controlador = 'front';
-    confirmar(nombre);
+function preguntasPlantilla(pregunta, opcion1, opcion2){
+    containerPreguntas.innerHTML = `
+        <h3>${pregunta}</h3>
+
+        <button class="button-respuesta" id="${opcion1}" onclick="seleccionar('${opcion1}');">${opcion1}</button>
+
+        <button class="button-respuesta" id="${opcion2}" onclick="seleccionar('${opcion2}');">${opcion2}</button>
+    `;
+    return;
+}
+
+function seleccionar(opcion){
+    controlador = opcion;
+    confirmar();
+}
+
+function si(){
+    controlador = 'si';
+    containerPreguntas.innerHTML = `
+        <h3>Que otra tecnologia te interesa ${nombre}</h3>
+        <input class="container__form__input" type="text" id="tecnologia" placeholder="Escribe aqui...">
+        <button class="button-respuesta" id="agregar" onclick="agregar();">Agregar</button>
+        <button class="button-respuesta" id="no" onclick="no();">No me interesa otra tecnologia</button>
+    `;
+    return;
+}
+
+function agregar(){
+    let tecnologia = document.getElementById('tecnologia').value;
+
+    tecnologias.push(tecnologia);
+
+    preguntasPlantilla(`Hay alguna otra tecnologia que te interese aprender ${nombre}?`,`si`, `no`);
+
+    return;
+}
+
+function no(){
+    controlador = 'no';
+
+    if(tecnologias.length === 0){
+        containerPreguntas.innerHTML = `
+            <h3>Excelente ${nombre}!</h3>
+        `;
+    } else {
+        containerPreguntas.innerHTML = `
+            <h3>Excelente ${nombre}!</h3>
+            <p>Sigue adelante y podras aprender estas tecnologias:</p>
+            <ul>
+            ${tecnologias.map(tecnologia => `<li>${tecnologia}</li>`).join('')}
+            </ul>
+        `;
+    }
+
+    return;
 }
 
